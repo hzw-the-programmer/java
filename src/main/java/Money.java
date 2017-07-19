4c7d397c8cfcade167b25e7dae76c717a8e47668
hzw <18676797056@163.com>
2017-07-19 22:21:45 +0800

    Addition, Finally

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -1,4 +1,4 @@
-public class Money {
+public class Money implements Expression {
   protected int amount;
   protected String currency;
 
@@ -33,7 +33,7 @@ public class Money {
     return amount + " " + currency();
   }
 
-  public Money plus(Money addend) {
+  public Expression plus(Money addend) {
     return new Money(amount + addend.amount, currency);
   }
 }
