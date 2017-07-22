f21d9a674309a35eb18fbc37b6a5dfa94120a1bc
hzw <18676797056@163.com>
2017-07-23 00:04:50 +0800

    Bank knows rate.

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -1,5 +1,10 @@
 public class Bank {
+
   public Money reduce(Expression source, String to) {
-    return source.reduce(to);
+    return source.reduce(this, to);
+  }
+
+  public int rate(String from, String to) {
+    return (from.equals("CHF") && to.equals("USD")) ? 2 : 1;
   }
 }
