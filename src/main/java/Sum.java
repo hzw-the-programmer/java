bb25bf233bf02201cbedaa029a140b0510da3c82
hzw <18676797056@163.com>
2017-07-25 22:30:27 +0800

    Mixed Currencies

--- src/main/java/Sum.java
+++ src/main/java/Sum.java
@@ -1,8 +1,8 @@
 public class Sum implements Expression {
-  Money augend;
-  Money addend;
+  Expression augend;
+  Expression addend;
 
-  public Sum(Money augend, Money addend) {
+  public Sum(Expression augend, Expression addend) {
     this.augend = augend;
     this.addend = addend;
   }
@@ -10,5 +10,9 @@ public class Sum implements Expression {
   public Money reduce(Bank bank, String to) {
     int amount = augend.reduce(bank, to).amount + addend.reduce(bank, to).amount;
     return new Money(amount, to);
+  }
+
+  public Expression plus(Expression addend) {
+    return null;
   }
 }
