078777172e57e0e548ae2b33f412ca0f0a441013
hzw <18676797056@163.com>
2017-07-25 22:13:18 +0800

    Mixed Currencies 1

--- src/test/java/DollarTest.java
+++ src/test/java/DollarTest.java
@@ -67,4 +67,14 @@ public class DollarTest {
   public void testIdentityRate() {
     assertEquals(1, new Bank().rate("USD", "USD"));
   }
+
+  @Test
+  public void testMixedAddition() {
+    Money fiveBucks = Money.dollar(5);
+    Money tenFrancs = Money.franc(10);
+    Bank bank = new Bank();
+    bank.addRate("CHF", "USD", 2);
+    Money result = bank.reduce(fiveBucks.plus(tenFrancs), "USD");
+    assertEquals(Money.dollar(10), result);
+  }
 }
